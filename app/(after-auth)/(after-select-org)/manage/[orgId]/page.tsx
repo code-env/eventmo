bd248a7b1737db0c3281791adc0c770a01edc44f
code-env <org.codeen@gmail.com>
2024-07-06 05:39:21 +0100

    feat: removed the log from the admin bar

--- app/(after-auth)/(after-select-org)/manage/[orgId]/page.tsx
+++ app/(after-auth)/(after-select-org)/manage/[orgId]/page.tsx
@@ -5,8 +5,6 @@ import React from "react";
 const OrgId = async ({ params }: { params: { orgId: string } }) => {
   const organizaiton = await useFindOrg(params.orgId);
 
-  console.log(organizaiton);
-
   if (!organizaiton)
     return (
       <div className="calc-height flex items-center justify-center">
